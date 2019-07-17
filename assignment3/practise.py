3f344fcd175018ddd77b078c7cab9b5829f15720
benjawan0109 <52033926+benjawan0109@users.noreply.github.com>
2019-07-17 22:30:51 +0700

    Create practise.py

---
+++ assignment3/practise.py
@@ -0,0 +1,9 @@
+mylist =[33, 21, 46]
+print(sum(mylist))
+
+mytuble = (9, 19, 29, 39, 49)
+print(mytuble)
+
+set1 = {3, 5, 12, 9, 20, 24, 26, 46, 65, 77}
+set2 = {5, 8, 9, 12, 13, 29, 77, 79, 85, 89}
+print(set1.intersection(set2))

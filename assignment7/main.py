82ab079d0f24e146080540d369da633d42676e1a
Computer Education <computereducation@Macmini-213-30.local>
2019-08-08 16:23:58 +0700

    update assignment7

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,4 +1,7 @@
 from flask import Flask,render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -6,14 +9,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('form.html')
-@app.route('/', methods=["POST"])
 
+@app.route('/', methods=["POST"])
 def chack():
     return 'Welcome to page...'
-@app.route('/', methods=["POST"])
 
-def h2():
-    return 'xyz';
+@app.route('/', methods=["POST"])
+def login():
+    return 'ok'
 
 if __name__ == '__main__':
     app.run()

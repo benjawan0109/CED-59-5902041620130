d6318eb2ce621e4b642e8bcc798d6cc775a5f35f
benjawan0109 <52033926+benjawan0109@users.noreply.github.com>
2019-08-22 08:26:55 +0700

    Update main.py

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,22 +1,32 @@
-from flask import Flask,render_template
+from flask import Flask,render_template , request
+from wtforms import StringField,PasswordField
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired, Length, Email
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'any secret string'
 
+class RegisterForm(FlaskForm):
+    name = StringField("name", validators=[InputRequired()])
+    last = StringField("last", validators=[InputRequired()])
+    email = StringField("Email",  [InputRequired("Please enter your email address."), Email("Please enter your email!.")])
+    username = StringField("username", validators=[InputRequired()])
+    password = PasswordField("password", validators=[InputRequired(), Length( message="Please enter your password!")])
 
 @app.route('/')
-def hello_world():
-    return render_template('form.html')
+def member():
+    form = RegisterForm()
+    return render_template('login.html', form=form)
 
-@app.route('/', methods=["POST"])
-def chack():
-    return 'Welcome to page...'
 
-@app.route('/', methods=["POST"])
-def login():
-    return 'ok'
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return "Register OK"
+    return render_template('login.html', form=form)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
